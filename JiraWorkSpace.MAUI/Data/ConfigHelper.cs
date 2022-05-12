d7de28503b201127c01600b1be26fdae54419e6e
zhangyuan <cool.zhangyy@gmail.com>
2022-05-13 00:47:34 +0800

    配置实现

---
+++ JiraWorkSpace.MAUI/Data/ConfigHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace JiraWorkSpace.MAUI.Data
+{
+    public static class ConfigHelper
+    {
+        private static ConfigModel Config;
+
+        public static ConfigModel GetConfig()
+        {
+            if (Config == null)
+                Config = JsonSerializer.Deserialize<ConfigModel>(File.ReadAllText($"{System.AppDomain.CurrentDomain.BaseDirectory}\\_c.json"));
+
+            return Config;
+        }
+    }
+
+    public class ConfigModel
+    {
+        public string AD { get; set; }
+        public string CodeDirectory { get; set; }
+        public string JiraDomain { get; set; }
+        public string JiraUaseName { get; set; }
+        public string JiraApiToken { get; set; }
+        public string JiraApiVersion { get; set; }
+    }
+}
